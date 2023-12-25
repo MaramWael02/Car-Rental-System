373485bf302511fd2da5ab6e4951c6a3385764e2
AnnTarek <es_Ann.Tarek@alexu.edu.eg>
2023-12-25 22:11:39 +0200

    added new input fields

--- my-react-app/src/components/register/Register.jsx
+++ my-react-app/src/components/register/Register.jsx
@@ -3,27 +3,107 @@ import React, { useState } from "react";
 export const REGISTER = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPass] = useState('');
-    const [name, setName] = useState('');
+    const [fname, setfName] = useState('');
+    const [lname, setlName] = useState('');
+    const [phoneNum, setPhoneNum] = useState('');
+    const [address, setaddress] = useState('');
+    const [username, setUsername] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault(); // prevents the page from refreshing
-        console.log(email);
+        
+        try {
+            const response = await fetch('http://localhost:8000/api/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password, phoneNum, fname, lname, address, username }),
+            });
 
+            if (response.ok) {
+                const data = await response.json();
+                console.log('Login successful:', data);
+                // Handle successful login (e.g., redirect to dashboard)
+            } else {
+                const errorData = await response.json();
+                setError(errorData.message || 'Login failed');
+            }
+        } catch (error) {
+            console.error('Error during login:', error);
+            setError('Hello Server error');
+        }
     }
 
     return (
         <div className="auth-form-container">
             <h2>Register</h2>
           <form className="register-form" onSubmit={handleSubmit}>
-             <label htmlFor="name">Full name</label>
-             <input value={name} onChange={(e) => setName(e.target.value)} name="name" id="name" placeholder="Your Name" />
-             <label htmlFor="email">Email</label>
-             <input value={email} onChange={(e) => setEmail(e.target.value)} type="Email" placeholder="youremail@gmail.com" id="email" name="email" />
-             <label htmlFor="password">Password</label>
-             <input value={password} onChange={(e) => setPass(e.target.value)} type="Password" placeholder="*********" id="password" name="password" />
-             <button type="submit">Sign up</button>
+
+            <label htmlFor="fname">First Name</label>
+            <input 
+                value={fname} 
+                onChange={(e) => setfName(e.target.value)} 
+                name="fname" id="fname" 
+                placeholder="Your First Name" 
+            />
+            <label htmlFor="lname">Last Name</label>
+            <input 
+                value={lname} 
+                onChange={(e) => setlName(e.target.value)} 
+                name="lname" id="lname" 
+                placeholder="Your Last Name" 
+            />
+
+            <label htmlFor="email">Email</label>
+                <input 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} 
+                type="Email"
+                placeholder="youremail@gmail.com"
+                id="email" name="email" 
+            />
+
+             <label htmlFor="phoneNum">Phone Number</label>
+             <input 
+                value={phoneNum} 
+                onChange={(e) => setPhoneNum(e.target.value)} 
+                type="tel"
+                placeholder="+(20)01000000000"
+                id="phoneNum" name="phoneNum" 
+            />
+
+            <label htmlFor="address">Address</label>
+            <input 
+                value={address} 
+                onChange={(e) => setaddress(e.target.value)} 
+                placeholder="city, country"
+                id="address" name="address" 
+            />
+            
+            <label htmlFor="username">Username</label>
+            <input 
+                value={username}
+                onChange={(e) => setUsername(e.target.value)} 
+                placeholder="abc123" 
+                id="username" name="username" 
+            />
+
+            <label htmlFor="password">Password</label>
+            <input 
+                value={password}
+                onChange={(e) => setPass(e.target.value)} 
+                type="Password"
+                placeholder="*********" 
+                id="password" name="password" 
+            />
+
+            <button type="submit">Sign up</button>
+            {error && <p className="error-message">{error}</p>}
          </form>
-         <button className="link-button" onClick={() => props.onFormSwitch('login')}>Already have an account? Sign in</button>
+         <button className="link-button" onClick={() => props.onFormSwitch('login')}>
+            Already have an account? Sign in
+         </button>
         </div>
     )
 }
