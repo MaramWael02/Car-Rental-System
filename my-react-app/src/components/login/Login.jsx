e3db31ec4d2e96db290c4fd9c4f05f77a4c7febe
MaramWael02 <es-MaramWael20252alexu.edu.eg>
2023-12-25 17:02:03 +0200

    backend-frontend-connection

--- my-react-app/src/components/login/Login.jsx
+++ my-react-app/src/components/login/Login.jsx
@@ -1,26 +1,67 @@
 import React, { useState } from "react";
 
+
 export const LOGIN = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPass] = useState('');
+    const [error, setError] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault(); // prevents the page from refreshing
-        console.log(email);
+        
+        try {
+            const response = await fetch('http://localhost:8000/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password }),
+            });
 
+            if (response.ok) {
+                const data = await response.json();
+                console.log('Login successful:', data);
+                // Handle successful login (e.g., redirect to dashboard)
+            } else {
+                const errorData = await response.json();
+                setError(errorData.message || 'Login failed');
+            }
+        } catch (error) {
+            console.error('Error during login:', error);
+            setError('Hello Server error');
+        }
     }
 
     return (
         <div className="auth-form-container">
             <h2>Login</h2>
-         <form className="register-form" onSubmit={handleSubmit}>
+            <form className="register-form" onSubmit={handleSubmit}>
                 <label htmlFor="email">Email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="Email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <input 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} 
+                    type="email" 
+                    placeholder="youremail@gmail.com" 
+                    id="email" 
+                    name="email" 
+                    required 
+                />
                 <label htmlFor="password">Password</label>
-                <input value={password} onChange={(e) => setPass(e.target.value)} type="Password" placeholder="*********" id="password" name="password" />
+                <input 
+                    value={password} 
+                    onChange={(e) => setPass(e.target.value)} 
+                    type="password" 
+                    placeholder="*********" 
+                    id="password" 
+                    name="password" 
+                    required 
+                />
                 <button type="submit">Login</button>
-         </form>
-         <button className="link-button" onClick={() => props.onFormSwitch('register')}>Don't have an account? Sign up</button>
+                {error && <p className="error-message">{error}</p>}
+            </form>
+            <button className="link-button" onClick={() => props.onFormSwitch('register')}>
+                Don't have an account? Sign up
+            </button>
         </div>
     )
 }
