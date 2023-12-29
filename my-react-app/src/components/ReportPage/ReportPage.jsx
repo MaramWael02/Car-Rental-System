5e3fea38a9a8aaa642ef218e642fe38ee3a0c29e
AnnTarek <es_Ann.Tarek@alexu.edu.eg>
2023-12-29 17:52:44 +0200

    created reportpage

---
+++ my-react-app/src/components/ReportPage/ReportPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header_reports from "../Header_reports/Header_reports";
+
+const ReportsPage = () => {
+
+
+  return (
+    <div >
+      <Header_reports />
+      <img alt="background of a car" src="../images/bg_1.jpg" className="background_image" />
+        <h1 className="ReportsLabel">Reports</h1>
+        
+        
+    </div>
+  );
+};
+
+export default ReportsPage;
